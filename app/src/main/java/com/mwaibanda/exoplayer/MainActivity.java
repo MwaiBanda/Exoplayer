ab91289363d2cf33985c48484637fecf3322f9af
Mwai Banda <bandamwai@gmail.com>
2022-05-17 01:00:58 -0500

    Added PlayerView & helix mp3 url

--- app/src/main/java/com/mwaibanda/exoplayer/MainActivity.java
+++ app/src/main/java/com/mwaibanda/exoplayer/MainActivity.java
@@ -3,14 +3,64 @@ package com.mwaibanda.exoplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.util.Util;
 
 public class MainActivity extends AppCompatActivity {
-
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
+    private boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playerView = findViewById(R.id.video_view);
+    }
+
+    private void initializePlayer() {
+        player = new SimpleExoPlayer.Builder(this).build();
+        MediaItem mediaItem = MediaItem.fromUri(getString(R.string.soundHelix_url));
+        player.setMediaItem(mediaItem);
+        playerView.setPlayer(player);
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+        player.prepare();
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        if(Util.SDK_INT >= 24)
+            initializePlayer();
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUI();
+        if (Util.SDK_INT < 24 || player == null) initializePlayer();
+    }
+    private void hideSystemUI() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+    private void releasePlayer() {
+        if (player != null) {
+            playWhenReady = player.getPlayWhenReady();
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            player.release();
+            player = null;
+        }
     }
 }
